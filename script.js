174d7c84facf05ce637d94e87d464deb849c8fac
DuDaria <60156592+DuDaria@users.noreply.github.com>
2020-03-15 17:01:17 +0400

    Update Homework_1 (new)
    Исправила новые замечания

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ var buttonOpen = document.querySelector(".slider__link_js");
 var buttonClose = document.querySelector(".modal-window__button_to_close_js");
 var input = document.querySelector(".modal-window__input_js");
 
-var colorTitle = document.querySelector(".footer__success-list_js");
-var addColor = document.querySelector(".footer__success-title_js");
-var delColor = document.querySelector(".footer__success-title_js");
-
 buttonOpen.addEventListener("click", function(){
   modal.classList.remove("modal-bg_close");
   input.focus();
@@ -25,6 +21,10 @@ window.addEventListener("keydown", function (event) {
 });
 
 // *************************************************
+// var colorTitle = document.querySelector(".footer__success-list_js");
+// var addColor = document.querySelector(".footer__success-title_js");
+// var delColor = document.querySelector(".footer__success-title_js");
+
 // delColor.classList.remove("footer__success-title_hover");
 
 // colorTitle.addEventListener("mouseenter", function(){
