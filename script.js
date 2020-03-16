d84febdbbf19fd007b81e2a8f02d61504aa72c6f
DuDaria <60156592+DuDaria@users.noreply.github.com>
2020-03-17 00:48:39 +0400

    Update Homework_2 (new)
    Исправила ошибки. Добавила favicon

--- script.js
+++ script.js
@@ -19,18 +19,3 @@ window.addEventListener("keydown", function (event) {
     buttonOpen.focus();
   }
 });
-
-// *************************************************
-// var colorTitle = document.querySelector(".footer__success-list_js");
-// var addColor = document.querySelector(".footer__success-title_js");
-// var delColor = document.querySelector(".footer__success-title_js");
-
-// delColor.classList.remove("footer__success-title_hover");
-
-// colorTitle.addEventListener("mouseenter", function(){
-//   addColor.classList.add("footer__success-title_hover");
-// });
-
-// colorTitle.addEventListener("mouseleave", function(){
-//   delColor.classList.remove("footer__success-title_hover");
-// });
